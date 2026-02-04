8bfe54e9c02fc3eb04f32739969ef313d484996a
DabluKumar <dk1965626@gmail.com>
2026-02-04 13:05:12 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+const note = []
+
+app.post("/notes", (req,body) =>{ 
+    console.log(req.body)
+    note.push(req.body)
+
+  console.log(note)
+
+    res.send("create note")
+})
+
+
+app.get("/notes" , (req,res) =>{
+    res.send(note)
+})
+
+app.delete("/notes/:index" , (req,res)=>{ 
+    // console.log(req.params.index)
+
+    delete note [req.params.index]
+    res.send("note delete successfully")
+})
+
+
+app.patch("/notes/:index" , (req,res)=>{ 
+    note[req.params.index].description = req.body.description
+    res.send("note update successfully")
+})
+
+module.exports =app
